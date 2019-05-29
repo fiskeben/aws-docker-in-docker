5c01cc52167cf57fff46fdae2ea32a152eca1450
Ricco Førgaard <ricco.forgaard@apparat.no>
2019-05-29 09:32:19 +0200

    Initial commit

---
+++ tag-image.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# This script will tag the "latest" image in a repository
+# with the supplied tag.
+#
+# The script takes two parameters:
+#
+# repo name: the name of the AWS ECR repo that holds the image
+# tag: the value to use as the tag
+#
+
+set -e
+
+usage() {
+        echo "Usage:"
+        echo "$(basename $0) reponame tag region"
+        exit 1
+}
+
+repo=$1
+tag=$2
+region=$3
+
+[ -z "$repo" ] && usage
+[ -z "$tag" ] && usage
+[ -z "$region" ] && usage
+
+echo "Tagging ${repo}:${tag} ($region)"
+
+MANIFEST=$(aws ecr batch-get-image --repository-name $repo --region $region --image-ids imageTag=latest --query 'images[].imageManifest' --output text)
+aws ecr put-image --repository-name $repo --image-tag $tag --region $region --image-manifest "$MANIFEST"
+
